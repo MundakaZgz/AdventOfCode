710be3d4552ef8850ac95dc7727e6a64a0ff474f
LG58UE <jose.antonio.herrero.moriana@ing.com>
2022-12-06 00:33:03 +0100

    Day 3 part 2 completed

---
+++ day3/partTwo.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  
+  priorities = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  sumOfPriorities = 0
+
+  lines = data.split(/\r?\n/)
+  
+  for (let i = 0; i < lines.length; i = i + 3) {
+    firstRucksack = lines[i].split('')
+    secondRucksack = lines[i+1].split('')
+    thirdRucksack = lines[i+2].split('')
+
+    foundLetter = ''
+
+    firstRucksack.forEach(letter => {
+        if (secondRucksack.indexOf(letter) != -1 && thirdRucksack.indexOf(letter) != -1) {
+            foundLetter = letter
+        }
+    });
+
+    sumOfPriorities += priorities.indexOf(foundLetter)
+    
+  }
+  
+  console.log(`The sum of priorities is ${sumOfPriorities}`)
+});
