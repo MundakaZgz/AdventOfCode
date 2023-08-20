74918bc3fa980ed0fefd8bd6e7070bd4cfa2db00
MundakaZgz <j.herrero.moriana@gmail.com>
2023-08-20 02:31:56 +0200

    Project refactorization continuation

--- src/2022/day5/partOne.js
+++ src/2022/day5/partOne.js
@@ -3,33 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function () {
-  const textFilePath = path.join(__dirname, 'input.txt');
+  function getData() {
+    const textFilePath = path.join(__dirname, 'input.txt');
+    const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
+    return data;
+  }
 
-  fs.readFile(textFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    const lines = data.split(/\r?\n/);
-    const crates = [];
-
-    for (let i = 0; i < (lines[0].length + 1) / 4; i++) {
-      crates.push([]);
-    }
-
-    loadCrates(lines, crates);
-    crates.map((x) => x.reverse());
-    moveCrates(lines, crates);
-
-    const topCrates = crates.map((x) => x.at(-1)).join('');
-    console.log(`The crates at the top are ${topCrates}`);
-  });
-
-  const loadCrates = (lines, crates) => {
+  function loadCrates(lines, crates) {
     const crateContent = /[A-Z]/gm;
     const isCrateContent = new RegExp(crateContent);
-
+    
     for (let i = 0; i < lines.length; i++) {
       const line = lines[i];
       if (line.length == 0) {
@@ -42,16 +25,16 @@ module.exports = function () {
         }
       }
     }
-  };
-
-  const moveCrates = (lines, crates) => {
+  }
+  
+  function moveCrates(lines, crates) {
     const movementRegex = /move (\d+) from (\d+) to (\d+)/;
     const movementRegexRecognizer = new RegExp(movementRegex);
     lines.forEach((line) => {
       const matches = movementRegexRecognizer.exec(line);
       if (matches) {
         const [numOfCrates, from, to] = matches.splice(1, 4).map(Number);
-
+        
         const accumulator = [];
         for (let i = 0; i < numOfCrates; i++) {
           const poped = crates[from - 1].pop();
@@ -60,5 +43,25 @@ module.exports = function () {
         crates[to - 1] = [...crates[to - 1], ...accumulator];
       }
     });
-  };
+  }
+  
+  function main() {
+    const crates = [];
+    
+    const lines = getData();
+    
+    for (let i = 0; i < (lines[0].length + 1) / 4; i++) {
+      crates.push([]);
+    }
+    loadCrates(lines, crates);
+    crates.map((x) => x.reverse());
+    moveCrates(lines, crates);
+    
+    const topCrates = crates.map((x) => x.at(-1)).join('');
+    console.log(`The crates at the top are ${topCrates}`);
+  }
+  
+  main();
+  
+
 };
