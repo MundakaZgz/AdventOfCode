5a3c4548d8962e33f336e43260f0022fb9a1acb6
MundakaZgz <j.herrero.moriana@gmail.com>
2023-08-20 02:25:14 +0200

    Project refactorization continuation

--- src/2022/day3/partOne.js
+++ src/2022/day3/partOne.js
@@ -2,18 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function () {
-  const textFilePath = path.join(__dirname, 'input.txt');
+  function getData() {
+    const textFilePath = path.join(__dirname, 'input.txt');
+    const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
+    return data;
+  }
 
-  fs.readFile(textFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
+  function main() {
     priorities = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     sumOfPriorities = 0;
 
-    data.split(/\r?\n/).forEach((rucksack) => {
+    const data = getData();
+
+    for (const rucksack of data) {
       compartment1 = rucksack.slice(0, rucksack.length / 2);
       compartment2 = rucksack.slice(-rucksack.length / 2);
       foundLetter = '';
@@ -21,10 +22,12 @@ module.exports = function () {
         if (compartment2.indexOf(letter) != -1) {
           foundLetter = letter;
         }
+        sumOfPriorities += priorities.indexOf(foundLetter);
       });
-      sumOfPriorities += priorities.indexOf(foundLetter);
-    });
+    }
 
     console.log(`The sum of priorities is ${sumOfPriorities}`);
-  });
+  }
+
+  main();
 };
--- src/2022/day3/partTwo.js
+++ src/2022/day3/partTwo.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function () {
-  const textFilePath = path.join(__dirname, 'input.txt');
 
-  fs.readFile(textFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  function getData() {
+    const textFilePath = path.join(__dirname, 'input.txt');
+    const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
+    return data;
+  }
 
+  function main() {
     priorities = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     sumOfPriorities = 0;
 
-    lines = data.split(/\r?\n/);
+    const lines = getData();
 
     for (let i = 0; i < lines.length; i += 3) {
       firstRucksack = lines[i].split('');
@@ -32,5 +32,8 @@ module.exports = function () {
     }
 
     console.log(`The sum of priorities is ${sumOfPriorities}`);
-  });
+  };
+
+  main();
+
 };
