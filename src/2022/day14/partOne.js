801a13a761b27cb1605f63f2efdc8c00e4274bfb
MundakaZgz <j.herrero.moriana@gmail.com>
2023-11-12 21:11:22 +0100

    Part one solved

--- src/2022/day14/partOne.js
+++ src/2022/day14/partOne.js
@@ -4,7 +4,7 @@ const path = require('path');
 const INITIAL_SAND_COORDINATES = { x: 500, y: 0 };
 
   function getData() {
-    const textFilePath = path.join(__dirname, 'test_input.txt');
+    const textFilePath = path.join(__dirname, 'input.txt');
     const data = fs
       .readFileSync(textFilePath, 'utf-8')
       .split('\n');
@@ -17,33 +17,54 @@ const INITIAL_SAND_COORDINATES = { x: 500, y: 0 };
     const xBegin = Math.min(from.y, to.y);
     const xEnd = Math.max(from.y, to.y);
 
-    for (let x = xBegin; x < xEnd; x++) {
-      for (let y = yBegin; y < yEnd; y++) {
-        map.add(`${x},${y}`);
+    for (let x = xBegin; x <= xEnd; x++) {
+      for (let y = yBegin; y <= yEnd; y++) {
+        map.add(`${y},${x}`);
       }
     }
 
     return map;
   }
 
+  extractListOfPointsFromSegment = (segment) => {
+    const points = [];
+    const regex = /(\d+),(\d+)/g;
+    let match;
+    while ((match = regex.exec(segment)) !== null) {
+      const x = parseInt(match[1]);
+      const y = parseInt(match[2]);
+      points.push({ x, y });
+    }
+    return points;
+  }
+
   drawPathsFromData = (data) => {
     let maxDepth = 0;
     let map = new Set();
-    for (const path of data) {
-      const points = path.split('->')
-        .map((step) => step.trim()).map((step) => {
-          [x, y] = step.split('');
-          maxDepth = Math.max(maxDepth, y);
-          return { x, y };
-        });
 
+    for (const segment of data) {
+      const points = extractListOfPointsFromSegment(segment);
       for (let i = 0; i < points.length - 1; i++) {
         map = markMapFromTo(map, points[i], points[i + 1]);
+        maxDepth = Math.max(maxDepth, points[i].y, points[i + 1].y);
       }
     }
-
-    maxDepth--;
     return {map, maxDepth};
+  }
+
+  function drawMap(map, maxDepth) {
+    let mapString = '';
+    for (let y = 0; y <= maxDepth; y++) {
+      for (let x = 494; x < 505; x++) {
+        if (map.has(`${x},${y}`)) {
+          mapString += '#';
+        } else {
+          mapString += '.';
+        }
+      }
+      mapString += '\n';
+    }
+    console.log(mapString);
   }
 
   function getInput() {
@@ -75,40 +96,38 @@ const INITIAL_SAND_COORDINATES = { x: 500, y: 0 };
     let fallingIntoTheEndlessVoid = false;
 
     while (!fallingIntoTheEndlessVoid) {
-      let sandUnitCoordinates = initialCoordinates;
+      let sandUnitCoordinates = { ...initialCoordinates };
+      let sandCanGoFurther = true;
       sandUnits++;
 
-      while (!fallingIntoTheEndlessVoid) {
+      while (sandCanGoFurther) {
         if (sandCanGoDown(map, sandUnitCoordinates)) {
-          console.log('sand can go down')
           sandUnitCoordinates.y++;
         } else if (sandCanGoDownLeft(map, sandUnitCoordinates)) {
-          console.log('sand can go down left')
           sandUnitCoordinates.x--;
           sandUnitCoordinates.y++;
         } else if (sandCanGoDownRight(map, sandUnitCoordinates)) {
-          console.log('sand can go down right')
           sandUnitCoordinates.x++;
           sandUnitCoordinates.y++;
         } else {
           map.add(`${sandUnitCoordinates.x},${sandUnitCoordinates.y}`);
-          break;
+          sandCanGoFurther = false;
         }
         if(sandUnitCoordinates.y >= maxDepth) {
           fallingIntoTheEndlessVoid = true;
+          sandCanGoFurther = false;
           sandUnits--;
         }
       }
     }
     
-    console.log(`sandUnits: ${sandUnits}`);
-
     return sandUnits;
   }
 
  function main() {
     let { map, maxDepth } = getInput();
     let sandUnits = dropAsManySandUnitsAsPossible(map, INITIAL_SAND_COORDINATES, maxDepth);
+    // drawMap(map, maxDepth);
     console.log(`${sandUnits} sand units could be dropped`);
   }
 
