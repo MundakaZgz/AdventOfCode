2f8c62ccc896f3edfdac5d1f5bbb800a4d2100da
jose.antonio.herrero.moriana <jose.antonio.herrero.moriana@ing.com>
2023-12-19 17:48:54 +0100

    Part 2 taking too much time, I need to try another approach

--- src/2023/day12/index.js
+++ src/2023/day12/index.js
@@ -1,10 +1,10 @@
 module.exports = () => {
-    const partOne = require('./partOne');
-    if (partOne) {
-      partOne();
-    }
-    // const partTwo = require('./partTwo');
-    // if (partTwo) {
-    //   partTwo();
+    // const partOne = require('./partOne');
+    // if (partOne) {
+    //   partOne();
     // }
+    const partTwo = require('./partTwo');
+    if (partTwo) {
+      partTwo();
+    }
   };
---
+++ src/2023/day12/partTwo.js
@@ -0,0 +1,125 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function () {
+  
+  function getData() {
+    const textFilePath = path.join(__dirname, 'test_input.txt');
+    const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
+    return data;
+  }
+  
+  function getConfigurationAndRecords(data) {
+    let output = {
+      configurations: [],
+      records: []
+    };
+    
+    for(let row=0; row<data.length; row++) {
+      let configuration = data[row].split(' ')[0].split('');
+      let record = data[row].split(' ')[1].split(',').map(record => parseInt(record));
+      output.configurations.push(configuration);
+      output.records.push(record);
+    }
+    
+    return output;
+  }
+  
+  function isValidArrangement(records) {
+    let regex = '^\\.*';
+
+    for(let i=0; i<records.length; i++) {
+      regex += `#{${records[i]}}`;
+      if(i < records.length-1) {
+        regex += '\\.+';
+      }
+    }
+    regex += '\\.*$';
+    
+    return new RegExp(regex);
+  }
+  
+  class Node {
+    constructor(value) {
+      this.value = value;
+      if(value === '?') {
+        this.left = new Node('.');
+        this.right = new Node('#');
+      } else {
+        this.left = null;
+        this.right = null;
+      }
+      
+    }
+  }
+  
+  function addValueToLeafs(value, node) {
+    if (!node.left) {
+        node.left = new Node(value === '?' ? '.' : value);
+    } else {
+        addValueToLeafs(value, node.left);
+    }
+
+    if (!node.right) {
+        node.right = new Node(value === '?' ? '#' : value);
+    } else {
+        addValueToLeafs(value, node.right);
+    }
+}
+  
+  function getPath(node, path, resultSet, validationFunction) {
+    if(!node) return;
+    
+    path += node.value === '?' ? '' : node.value;
+    if(node.left === null && node.right === null && validationFunction.test(path)) {
+      resultSet.add(path);
+    }
+    getPath(node.left, path, resultSet, validationFunction);
+    getPath(node.right, path, resultSet, validationFunction);
+    
+  }
+  
+  function getArrangements(configuration, validationFunction) {
+    
+    let configTreeRoot = new Node(configuration[0]);
+
+    for(let i=1; i<configuration.length; i++) {
+      addValueToLeafs(configuration[i], configTreeRoot);
+    }
+  
+    let resultSet = new Set();
+    getPath(configTreeRoot, '', resultSet, validationFunction);
+    
+    return  [...resultSet];
+  }
+  
+  
+  const main = () => {
+    let data = getData();
+    let configurationsAndRecords = getConfigurationAndRecords(data);
+    let arrangements = [];
+    for(let i=0; i<configurationsAndRecords.configurations.length; i++) {
+      let configuration = configurationsAndRecords.configurations[i];
+      let records = configurationsAndRecords.records[i];
+      let validationFunction = isValidArrangement(records);
+      let arrangement = getArrangements(configuration, validationFunction);
+      let combinations = [...arrangement];
+      for(let times = 0; times < 4; times++) {
+          let newCombinations = new Set();
+          for(let i=0; i<combinations.length; i++) {
+              for(let j=0; j<arrangement.length; j++) {
+                  newCombinations.add(combinations[i] + arrangement[j]);
+              }
+          }
+          combinations = [...newCombinations].filter((x) => !/#{4}/g.test(x));
+      }
+
+      arrangements.push(combinations);
+    }
+    
+    console.log(`The sum of all possible arrangements is ${arrangements.map((x) => x.length).reduce((a,b) => a+b, 0)}`)
+  };
+  
+  main();
+};
+
