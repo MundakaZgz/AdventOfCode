8a512cb3c83afafc611dc35363d8771de25c6e34
jose.antonio.herrero.moriana <jose.antonio.herrero.moriana@ing.com>
2023-12-20 08:23:30 +0100

    AoC Day 12 part two solved

--- src/2023/day12/partTwo.js
+++ src/2023/day12/partTwo.js
@@ -1,123 +1,52 @@
 const fs = require('fs');
+const { get } = require('http');
 const path = require('path');
 
 module.exports = function () {
   
   function getData() {
-    const textFilePath = path.join(__dirname, 'test_input.txt');
+    const textFilePath = path.join(__dirname, 'input.txt');
     const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
     return data;
   }
   
-  function getConfigurationAndRecords(data) {
-    let output = {
-      configurations: [],
-      records: []
-    };
+  function getConfigurationAndRecords(line) {
+    let configuration = line.split(' ')[0];
+    let record = line.split(' ')[1].split(',').map(record => parseInt(record));
     
-    for(let row=0; row<data.length; row++) {
-      let configuration = data[row].split(' ')[0].split('');
-      let record = data[row].split(' ')[1].split(',').map(record => parseInt(record));
-      output.configurations.push(configuration);
-      output.records.push(record);
-    }
-    
-    return output;
+    return [configuration, record];
   }
   
-  function isValidArrangement(records) {
-    let regex = '^\\.*';
+  
+  function trimStart(str) {
+    while (str.startsWith(".")) {
+      str = str.substring(1);
+    }
+    return str;
+  }
 
-    for(let i=0; i<records.length; i++) {
-      regex += `#{${records[i]}}`;
-      if(i < records.length-1) {
-        regex += '\\.+';
-      }
-    }
-    regex += '\\.*$';
-    
-    return new RegExp(regex);
+  let cache = {};
+  
+  function findCombinations(configuration, records) {
+    const line = configuration + " " + records.join(",");
+    if (cache[line]) return cache[line];
+    if (records.length <= 0) return Number(!configuration.includes("#"));
+    if (configuration.length - records.reduce((a, b) => a + b) - records.length + 1 < 0) return 0;
+    const damagedOrUnknown = !configuration.slice(0, records[0]).includes(".");
+    if (configuration.length == records[0]) return Number(damagedOrUnknown);
+    return cache[line] ??= (configuration[0] != "#" ? findCombinations(trimStart(configuration.slice(1)), records) : 0) +
+    (damagedOrUnknown && configuration[records[0]] != "#" ? findCombinations(trimStart(configuration.slice(records[0] + 1)), records.slice(1)) : 0);
   }
-  
-  class Node {
-    constructor(value) {
-      this.value = value;
-      if(value === '?') {
-        this.left = new Node('.');
-        this.right = new Node('#');
-      } else {
-        this.left = null;
-        this.right = null;
-      }
-      
-    }
-  }
-  
-  function addValueToLeafs(value, node) {
-    if (!node.left) {
-        node.left = new Node(value === '?' ? '.' : value);
-    } else {
-        addValueToLeafs(value, node.left);
-    }
-
-    if (!node.right) {
-        node.right = new Node(value === '?' ? '#' : value);
-    } else {
-        addValueToLeafs(value, node.right);
-    }
-}
-  
-  function getPath(node, path, resultSet, validationFunction) {
-    if(!node) return;
-    
-    path += node.value === '?' ? '' : node.value;
-    if(node.left === null && node.right === null && validationFunction.test(path)) {
-      resultSet.add(path);
-    }
-    getPath(node.left, path, resultSet, validationFunction);
-    getPath(node.right, path, resultSet, validationFunction);
-    
-  }
-  
-  function getArrangements(configuration, validationFunction) {
-    
-    let configTreeRoot = new Node(configuration[0]);
-
-    for(let i=1; i<configuration.length; i++) {
-      addValueToLeafs(configuration[i], configTreeRoot);
-    }
-  
-    let resultSet = new Set();
-    getPath(configTreeRoot, '', resultSet, validationFunction);
-    
-    return  [...resultSet];
-  }
-  
-  
   const main = () => {
     let data = getData();
-    let configurationsAndRecords = getConfigurationAndRecords(data);
-    let arrangements = [];
-    for(let i=0; i<configurationsAndRecords.configurations.length; i++) {
-      let configuration = configurationsAndRecords.configurations[i];
-      let records = configurationsAndRecords.records[i];
-      let validationFunction = isValidArrangement(records);
-      let arrangement = getArrangements(configuration, validationFunction);
-      let combinations = [...arrangement];
-      for(let times = 0; times < 4; times++) {
-          let newCombinations = new Set();
-          for(let i=0; i<combinations.length; i++) {
-              for(let j=0; j<arrangement.length; j++) {
-                  newCombinations.add(combinations[i] + arrangement[j]);
-              }
-          }
-          combinations = [...newCombinations].filter((x) => !/#{4}/g.test(x));
-      }
-
-      arrangements.push(combinations);
+    let result = 0;
+    
+    for (const line of data) {
+      const [configuration, records] = getConfigurationAndRecords(line);
+      result += findCombinations(Array(5).fill(configuration).join("?"), Array(5).fill(records).flat());
     }
     
-    console.log(`The sum of all possible arrangements is ${arrangements.map((x) => x.length).reduce((a,b) => a+b, 0)}`)
+    console.log(`The sum of all possible arrangements is ${result}`)
   };
   
   main();
