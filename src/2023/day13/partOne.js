ac545f3a74b5e992387effe3351cd8a8cc2d31ed
MundakaZgz <j.herrero.moriana@gmail.com>
2023-12-23 11:39:47 +0100

    Closer to the solution

--- src/2023/day13/partOne.js
+++ src/2023/day13/partOne.js
@@ -8,44 +8,60 @@ module.exports = function () {
     const data = fs.readFileSync(textFilePath, 'utf8').split(/\r?\n/);
     return data;
   }
-
-  function transpose(matrix) {
-    let arrayOfArrays = matrix.map(row => row.split(''));
-    let transposedMatrix = arrayOfArrays[0].map((col, i) => arrayOfArrays.map(row => row[i]));
-    return transposedMatrix.map(row => row.join(''));
-  }
-
+  
   function findMirrorIndexes(matrix) {
     let possibleIndexes = [];
-
+    
     for(let i=0; i<matrix.length-1; i++) {
-        let row = matrix[i];
-        let rowArray = row.split('');
-        let rowStringReversed = rowArray.toReversed();
+      let row = matrix[i];
+      let rowArray = row.split('');
+      
+      let newPossibleIndexes = [];
+      for(let j=0; j<rowArray.length; j++) {
         let thereIsMirror = true;
-        let distanceToMirror = Math.min(i, row.length - i - 1);
-
-        for(let j=0; j<distanceToMirror; j++) {
-            if(rowArray[i-j] !== rowStringReversed[j+i+1]) {
-                thereIsMirror = false;
-                break;
-            }
+        let distanceToMirror = Math.min(j, rowArray.length - j - 1);
+        
+        if(distanceToMirror === 0) {
+          continue;
         }
-
+        
+        for(let k=0; k<distanceToMirror; k++) {
+          if(rowArray[j-k] !== rowArray[j+k+1]) {
+            thereIsMirror = false;
+            break;
+          }
+        }
+        
         if(thereIsMirror) {
-            possibleIndexes.push(i);
-        } else {
-            if(possibleIndexes.indexOf(i) > -1) {
-                possibleIndexes.splice(possibleIndexes.indexOf(i), 1);
-            }
+          newPossibleIndexes.push(j);
         }
+      }
+      
+      // Intersect newPossibleIndexes with possibleIndexes
+      if(i === 0) {
+        possibleIndexes = newPossibleIndexes;
+      } else {
+        possibleIndexes = possibleIndexes.filter(index => newPossibleIndexes.includes(index));
+      }
     }
-
-    // 1-based indexes
+    
+    // update to 1-based index
     possibleIndexes = possibleIndexes.map(index => index + 1);
-    console.log(possibleIndexes);
-
+    
     return possibleIndexes;
+  }
+  
+  function transposeMatrix(matrix) {
+    let newMatrix = [];
+    newMatrix =  matrix.map(row => row.split(''));
+    newMatrix = newMatrix[0].map((col, i) => newMatrix.map(row => row[i]));
+    return newMatrix.map(row => row.join(''));
+  }
+  
+  printMatrix = (matrix) => {
+    for(const row of matrix) {
+      console.log(row);
+    }
   }
   
   const main = () => {
@@ -54,24 +70,28 @@ module.exports = function () {
     
     let pattern = [];
     let numberOfColumnsToTheLeft = 0;
-    let numberOfColumnsAbove = 0;
-
+    let numberOfRowsAbove = 0;
+    let numberOfPattern = 0;
+    
     for(const line of data) {
-        if(line === '') {
-            numberOfColumnsToTheLeft += findMirrorIndexes(pattern).reduce((a, b) => a + b);
-            pattern = transpose(pattern);
-            numberOfColumnsAbove += findMirrorIndexes(pattern).reduce((a, b) => a + b);
-            pattern = [];
-        } else {
-            pattern.push(line);
+      if(line === '') {
+        if(numberOfPattern % 2 == 0) {
+          numberOfColumnsToTheLeft += findMirrorIndexes(pattern).reduce((a, b) => a + b, 0);
+        } else {    
+          pattern = transposeMatrix(pattern);
+          numberOfRowsAbove += findMirrorIndexes(pattern).reduce((a, b) => a + b, 0);
         }
+        pattern = [];
+        numberOfPattern++;
+      } else {
+        pattern.push(line);
+      }
     }
-
-    let result = numberOfColumnsToTheLeft + 100 * numberOfColumnsAbove;
+    
+    let result = numberOfColumnsToTheLeft + 100 * numberOfRowsAbove;
     
     console.log(`The number I get after summarizing all of your notes is ${result}`)
   };
   
   main();
 };
-
