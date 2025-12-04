270a2bee08e3c87f35f039695df39a6e2cb102a3
MundakaZgz <j.herrero.moriana@gmail.com>
2025-12-04 20:44:02 +0100

    Year 2025 Day 1 part 2 completed

--- src/2025/day01/index.js
+++ src/2025/day01/index.js
@@ -34,7 +34,52 @@ async function resolveFirstChallenge(input) {
 }
 
 async function resolveSecondChallenge(input) {
-  console.log('Second challenge not implemented')
+  let totalTimesAt0 = 0
+  let dial = 50
+  
+  console.log(`The dial starts pointing at ${dial}`)
+  for (const line of input.split('\n')) {
+    const direction = line[0]
+    let value = parseInt(line.slice(1), 10)
+    let timesAt0 = 0 
+
+    if (value > 100) {
+      // Count full rotations
+      timesAt0 += Math.floor(value / 100)
+      value = value % 100
+    }
+
+    if(direction === 'L') {
+      if(dial - value < 0 && dial !== 0) {
+        timesAt0++
+      }
+      dial = dial - value
+      if(dial < 0) {
+        dial += 100
+      }
+    }
+
+    if(direction === 'R') {
+      if(dial + value  > 100 && dial !== 0) {
+        timesAt0++
+      }
+      dial = dial + value
+      if(dial >= 100) {
+        dial -= 100
+      }
+    }
+
+    if (dial === 0) {
+      // end of a rotation
+      timesAt0++
+    }
+
+    totalTimesAt0 += timesAt0
+
+    console.log(`The dial is rotated ${line} to point at ${dial}` + (timesAt0 > 0 && dial !== 0 ? ` (during this rotation, it points at 0 ${timesAt0}.` : '') )
+  }
+  console.log('Times passing by 0:', totalTimesAt0)
 }
+
 
 run()
